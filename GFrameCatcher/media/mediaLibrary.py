b989f4cde98468d37c3dd1f54bf86e3742c9c5fd
Tim Babych <tim.babych@gmail.com>
2016-06-12 00:30:25 +0300

    whitespace

--- GFrameCatcher/media/mediaLibrary.py
+++ GFrameCatcher/media/mediaLibrary.py
@@ -41,7 +41,7 @@ import time
 import datetime
 import os
 
-class CaptureType :
+class CaptureType:
     SECONDS = 0
     FRAMES = 1
 
@@ -70,76 +70,80 @@ class MediaInfo(gobject.GObject) :
                 gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, [
                 gobject.TYPE_STRING, gobject.TYPE_STRING])
             }
+
     def __init__(self):
-       gobject.GObject.__init__(self)
-       self.size = (128, 128)
+        gobject.GObject.__init__(self)
+        self.size = (128, 128)
        
     def openfile(self, filename ,aModel):
-       self.images = list()
-       self.errors = list()
-       self.fileName = filename
-       self.codec = ""
-       if (self.player != None):
-           self.player.set_state(gst.STATE_NULL)
+        self.images = list()
+        self.errors = list()
+        self.fileName = filename
+        self.codec = ""
+        if (self.player != None):
+            self.player.set_state(gst.STATE_NULL)
            
-       self.__isEndOfStream = False
-       self.player = gst.element_factory_make("playbin", "player")
-       videoBin = gst.Bin("video")
-       videoFilter = gst.element_factory_make("capsfilter", "videofilter")
-       videoBin.add(videoFilter)
-       videoFilter.set_property("caps", gst.Caps("video/x-raw-rgb, depth=24, bpp=24"))
-       ghostPad = gst.GhostPad("sink", videoFilter.get_pad("sink"))
-       videoBin.add_pad(ghostPad)
-       videoSink = gst.element_factory_make("fakesink", "videosink")
-       videoBin.add(videoSink)
-       pad = videoSink.get_pad("sink")
-       pad.add_buffer_probe(self.__onBufferProbe)
-       gst.element_link_many(videoFilter, videoSink)
-       self.player.set_property("video-sink", videoBin)
-       
-       self.bus = self.player.get_bus()
-       self.bus.add_signal_watch()
-       self.watchID = self.bus.connect("message", self.__onMessage)
-       self.player.set_property("uri", "file://" + filename)
-       self.player.set_state(gst.STATE_PAUSED)
-       self.model = aModel
-    def __seek(self, seek_pos):
-       event = self.player.seek(1.0, gst.FORMAT_TIME,
-       gst.SEEK_FLAG_FLUSH | gst.SEEK_FLAG_ACCURATE,
-       gst.SEEK_TYPE_SET, seek_pos,
-       gst.SEEK_TYPE_NONE, 0)
-       print seek_pos
-    def __onBufferProbe(self, pad, buffer):
-       if (self.player != None and self.__isEndOfStream == False):
-           caps = buffer.caps
-           if (caps == None):
-               print _("Stream returned no caps, exiting")
-               sys.exit(6)
-           filters = caps[0]
-           self.width = filters["width"]
-           self.height = filters["height"]
-           pixbuf = gtk.gdk.pixbuf_new_from_data(buffer.data, gtk.gdk.COLORSPACE_RGB, False, 8, self.width, self.height, self.width * 3)
-           self.__duration =  self.player.query_duration(gst.FORMAT_TIME)[0]/gst.SECOND
-           position = self.player.query_position(gst.FORMAT_TIME)[0]/gst.SECOND
-           frame = pixbuf,time.strftime("%H:%M:%S", time.gmtime(datetime.timedelta(seconds= position).seconds))
-           self.images.append(frame)
-           scaled = pixbuf.scale_simple(self.size[0], self.size[1], gtk.gdk.INTERP_NEAREST)
-           self.model.append([scaled, frame[1]])
+        self.__isEndOfStream = False
+        self.player = gst.element_factory_make("playbin", "player")
+        videoBin = gst.Bin("video")
+        videoFilter = gst.element_factory_make("capsfilter", "videofilter")
+        videoBin.add(videoFilter)
+        videoFilter.set_property("caps", gst.Caps("video/x-raw-rgb, depth=24, bpp=24"))
+        ghostPad = gst.GhostPad("sink", videoFilter.get_pad("sink"))
+        videoBin.add_pad(ghostPad)
+        videoSink = gst.element_factory_make("fakesink", "videosink")
+        videoBin.add(videoSink)
+        pad = videoSink.get_pad("sink")
+        pad.add_buffer_probe(self.__onBufferProbe)
+        gst.element_link_many(videoFilter, videoSink)
+        self.player.set_property("video-sink", videoBin)
 
-           if(self.captureType == CaptureType.SECONDS) :
-               if(position + self.seconds) <= self.__duration:
-                   self.__seek((position + self.seconds) * gst.SECOND)
-               else:
-                   self.player.send_event(gst.event_new_eos())
-                   self.__isEndOfStream = True
-           else :
-               if(len(self.images) < self.frames):
-                   self.__seek((position + (self.__duration / (self.frames - 1))) * gst.SECOND)
-               else:
-                   self.player.send_event(gst.event_new_eos())
-                   self.__isEndOfStream = True
+        self.bus = self.player.get_bus()
+        self.bus.add_signal_watch()
+        self.watchID = self.bus.connect("message", self.__onMessage)
+        self.player.set_property("uri", "file://" + filename)
+        self.player.set_state(gst.STATE_PAUSED)
+        self.model = aModel
+
+    def __seek(self, seek_pos):
+        event = self.player.seek(1.0, gst.FORMAT_TIME,
+        gst.SEEK_FLAG_FLUSH | gst.SEEK_FLAG_ACCURATE,
+        gst.SEEK_TYPE_SET, seek_pos,
+        gst.SEEK_TYPE_NONE, 0)
+        print seek_pos
+
+    def __onBufferProbe(self, pad, buffer):
+        if (self.player != None and self.__isEndOfStream == False):
+            caps = buffer.caps
+            if (caps == None):
+                print _("Stream returned no caps, exiting")
+                sys.exit(6)
+            filters = caps[0]
+            self.width = filters["width"]
+            self.height = filters["height"]
+            pixbuf = gtk.gdk.pixbuf_new_from_data(buffer.data, gtk.gdk.COLORSPACE_RGB, False, 8, self.width, self.height, self.width * 3)
+            self.__duration =  self.player.query_duration(gst.FORMAT_TIME)[0]/gst.SECOND
+            position = self.player.query_position(gst.FORMAT_TIME)[0]/gst.SECOND
+            frame = pixbuf,time.strftime("%H:%M:%S", time.gmtime(datetime.timedelta(seconds= position).seconds))
+            self.images.append(frame)
+            scaled = pixbuf.scale_simple(self.size[0], self.size[1], gtk.gdk.INTERP_NEAREST)
+            self.model.append([scaled, frame[1]])
+
+            if(self.captureType == CaptureType.SECONDS) :
+                if (position + self.seconds) <= self.__duration:
+                    self.__seek((position + self.seconds) * gst.SECOND)
+                else:
+                    self.player.send_event(gst.event_new_eos())
+                    self.__isEndOfStream = True
+            else :
+                if(len(self.images) < self.frames):
+                    self.__seek((position + (self.__duration / (self.frames - 1))) * gst.SECOND)
+                else:
+                    self.player.send_event(gst.event_new_eos())
+                    self.__isEndOfStream = True
            percentage = float(100 * position) / float(self.__duration)
            self.emit("progress", percentage)
+
     def __onMessage(self, bus, message):
         if (message.type == gst.MESSAGE_ELEMENT):
             if(message.structure.get_name().startswith("missing-")):
@@ -162,6 +166,7 @@ class MediaInfo(gobject.GObject) :
             self.emit("completed")
         elif (message.type == gst.MESSAGE_EOS):
             self.emit("completed")
+
     def __getMissingElement(self, message, window_id = 0):
         if gst.pygst_version < (0, 10, 10):
             print _("This version of gstreamer can't handle missing elements")
@@ -184,12 +189,16 @@ class MediaInfo(gobject.GObject) :
             self.emit("error", result.value_name, "\n".join(self.errors))
     def __getGStreamerLibraryVersion(self):
         return ".".join([str(integer) for integer in gst.pygst_version])
+
     def getDuration(self):
         return time.strftime("%H:%M:%S",time.gmtime(datetime.timedelta(seconds= self.__duration).seconds))
+
     def getDurationSeconds(self):
         return self.__duration
+
     def getFilename(self):
         return os.path.basename(self.fileName)
+
     def cancel(self):
         if (self.player != None):
            self.player.set_state(gst.STATE_NULL)
@@ -198,4 +207,5 @@ class MediaInfo(gobject.GObject) :
         self.watchID = None
 
     GStreamerLibraryVersion = property(__getGStreamerLibraryVersion)
+
 gobject.type_register(MediaInfo)
